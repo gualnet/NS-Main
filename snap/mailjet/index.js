a409dceff06eca2cac549b6043ee7d8157c85473
gualnet <galy@student.42.fr>
2022-05-10 18:17:45 +0200

    Feat (*): implement reset password process (#74)
    * update user type
    * add modal to request password reset on signup.html
    * add end point for reset password request
    * add reset password mail template + new mailer function
    * upgrade reset password modal ux
    * implementation de la page de password recovery
    * implement set new user password end point
    * update reset user password email template
    * register new uri to pwd-recover page

--- snap/mailjet/index.js
+++ snap/mailjet/index.js
@@ -1,88 +1,138 @@
 //"bcfc7e92566a91abaf67278f0838225f", "e23c11e01acf44a76ffeb6cfe95bc2d1"
 
 async function sendEmail(_entityId, _toEmail, _toName, _subject, _msg) {
-    var entity = await STORE.enititymgmt.getEntityById(_entityId);
-    if (entity) {
-        var mailjet = require("node-mailjet").connect(entity.mailjet_apikey, entity.mailjet_secretkey);
-        const request = mailjet
-            .post("send", { 'version': 'v3.1' })
-            .request({
-                "Messages": [
-                    {
-                        "From": {
-                            "Email": 'noreply@nauticspot.fr',
-                            "Name": entity.name 
-                        },
-                        "To": [
-                            {
-                                "Email": _toEmail,
-                                "Name": _toName
-                            }
-                        ],
-                        "Subject": _subject,
-                        "TextPart": _msg,
-                    }
-                ]
-            })
-        request
-            .then((result) => {
-                console.log('[MAILJET INFO]', result.body);
-            })
-            .catch((err) => {
-                console.log('[MAILJET ERROR]', err);
-            })
-    }
+	var entity = await STORE.enititymgmt.getEntityById(_entityId);
+	if (entity) {
+		var mailjet = require("node-mailjet").connect(entity.mailjet_apikey, entity.mailjet_secretkey);
+		const request = mailjet
+			.post("send", { 'version': 'v3.1' })
+			.request({
+				"Messages": [
+					{
+						"From": {
+							"Email": 'noreply@nauticspot.fr',
+							"Name": entity.name
+						},
+						"To": [
+							{
+								"Email": _toEmail,
+								"Name": _toName
+							}
+						],
+						"Subject": _subject,
+						"TextPart": _msg,
+					}
+				]
+			})
+		request
+			.then((result) => {
+				console.log('[MAILJET INFO]', result.body);
+			})
+			.catch((err) => {
+				console.log('[MAILJET ERROR]', err);
+			})
+	}
 }
 
 async function sendHTMLEmail(_entityId, _toEmail, _toName, _subject, _msg) {
-    var entity = await STORE.enititymgmt.getEntityById(_entityId);
-    if (entity) {
-        var mailjet = require("node-mailjet").connect(entity.mailjet_apikey, entity.mailjet_secretkey);
-        const request = mailjet
-            .post("send", { 'version': 'v3.1' })
-            .request({
-                "Messages": [
-                    {
-                        "From": {
-                            // "Email": entity.mailjet_mail
-                            "Email": 'noreply@nauticspot.fr',
-                            "Name": entity.name
-                            },
-                        "To": [
-                            {
-                                "Email": _toEmail,
-                                "Name": _toName
-                            }
-                        ],
-                        "Subject": _subject,
-                        "HTMLPart": _msg,
-                    }
-                ]
-            })
-        request
-            .then((result) => {
-                console.log('[MAILJET INFO]', result.body);
-            })
-            .catch((err) => {
-                console.log('[MAILJET ERROR]', err)
-            })
-    }
+	var entity = await STORE.enititymgmt.getEntityById(_entityId);
+	if (entity) {
+		var mailjet = require("node-mailjet").connect(entity.mailjet_apikey, entity.mailjet_secretkey);
+		const request = mailjet
+			.post("send", { 'version': 'v3.1' })
+			.request({
+				"Messages": [
+					{
+						"From": {
+							// "Email": entity.mailjet_mail
+							"Email": 'noreply@nauticspot.fr',
+							"Name": entity.name
+						},
+						"To": [
+							{
+								"Email": _toEmail,
+								"Name": _toName
+							}
+						],
+						"Subject": _subject,
+						"HTMLPart": _msg,
+					}
+				]
+			})
+		request
+			.then((result) => {
+				console.log('[MAILJET INFO]', result.body);
+			})
+			.catch((err) => {
+				console.log('[MAILJET ERROR]', err)
+			})
+	}
+}
+
+/**
+ * @typedef mailerAddressDetails
+ * @property {string} email
+ * @property {string} name
+ */
+/**
+ * @typedef mailerMsgDetails
+ * @property {string} subject
+ * @property {string || undefined} HTMLPart
+ * @property {string || undefined} TextPart
+ */
+/**
+ * Send Email - but function a little more flex
+ * @param {mailerAddressDetails} fromDetails 
+ * @param {mailerAddressDetails} toDetails 
+ * @param {mailerMsgDetails} msgDetails
+ */
+const sendMailRaw = async (fromDetails, toDetails, msgDetails) => {
+	if (!toDetails.email || !toDetails.name) throw new Error('Invalide toDetails params');
+
+	const API_KEY = OPTION.MAILJET_API_KEY;
+	const SECRET_KEY = OPTION.MAILJET_SECRET_KEY;
+
+	const mailjet = require('node-mailjet');
+	const mailer = mailjet.connect(API_KEY, SECRET_KEY);
+
+	const response = await mailer
+		.post("send", { version: 'v3.1' })
+		.request({
+			"Messages": [{
+				"From": {
+					"Email": fromDetails?.email || OPTION.MAILJET_SENDER_EMAIL,
+					"Name": fromDetails?.name || 'Nauticspot',
+				},
+				"To": [{
+					"Email": toDetails.email,
+					"Name": toDetails.name,
+				}],
+				...msgDetails,
+				// todo implem attachment later
+				// "Attachments": [{
+				// 	"ContentType": "application/json",
+				// 	"Filename": fileName,
+				// 	"Base64Content": content
+				// }],
+			}],
+		});
 }
 
 exports.handler = async (req, res) => {
-    if (req.method == "POST") {
-        var _to = req.post.email;
-        var _name = req.post.name;
-        var _msg = req.post.msg;
-        var _subject = req.post.subject;
+	if (req.method == "POST") {
+		var _to = req.post.email;
+		var _name = req.post.name;
+		var _msg = req.post.msg;
+		var _subject = req.post.subject;
 
-        sendEmail(_to, _name, _subject, _msg);
-    }
-    res.end();
+		sendEmail(_to, _name, _subject, _msg);
+	}
+	res.end();
 }
 
 exports.store =
 {
-    send: sendEmail,
-    sendHTML: sendHTMLEmail,
+	send: sendEmail,
+	sendHTML: sendHTMLEmail,
+	sendMailRaw,
 }
--- types.js
+++ types.js
@@ -198,7 +198,11 @@ const incidentsTypes = require('./snap/lib-js/enums').incidentsTypes;
 
 /**
  * @typedef T_user - users referenced in nauticspot user db -> mobile app access
- * @property {EpochTimeStamp} date
+ * @property {ENUM.rolesMobileApp} roleMobileApp
+ * @property {EpochTimeStamp} created_at 
+ * @property {EpochTimeStamp} date - deprec
+ * @property {EpochTimeStamp} updated_at 
+ * @property {string || undefined} resetPwdToken - 
  * @property {string} category
  * @property {string} contract_number
  * @property {string} email
@@ -211,22 +215,23 @@ const incidentsTypes = require('./snap/lib-js/enums').incidentsTypes;
  * @property {string} prefix
  * @property {string} prefixed_phone
  * @property {string} token
- * @property {ENUM.rolesMobileApp} roleMobileApp
  */
 
 /**
  * @typedef T_userFP - users referenced in fortpress user db -> dashboard access
- * @property {string} id
- * @property {string} login
- * @property {string} pw_type
- * @property {string} password
- * @property {string} name
+ * @property {EpochTimeStamp} created_at 
+ * @property {EpochTimeStamp} updated_at 
  * @property {string} bio
+ * @property {string} id
+ * @property {string} last_login
+ * @property {string} link
+ * @property {string} login
+ * @property {string} name
+ * @property {string} password
+ * @property {string} photo
+ * @property {string} pw_type
  * @property {string} role - Used by fortpress
  * @property {T_data_userFP} data
- * @property {string} link
- * @property {string} last_login
- * @property {string} photo
  */
 /**
  * @typedef T_data_userFP
