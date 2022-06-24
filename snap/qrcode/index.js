06ca64580509114c84a1e4081d0e1f02b24b6664
gualnet <galy@student.42.fr>
2022-06-24 12:13:11 +0200

    Feat (qrcode snap): Activate qrcode links to app store (#113)
    * Promises refacto
    * Add apple & android fields on dashboard qrcode page
    * Add automatic redirection to stores on landing page
    * Add apple & android link support in qrcode snap

--- snap/qrcode/index.js
+++ snap/qrcode/index.js
@@ -1,3 +1,4 @@
+const TYPES = require('../../types');
 const ENUM = require('../lib-js/enums');
 const { verifyRoleAccess } = require('../lib-js/verify');
 
@@ -20,13 +21,14 @@ var _userCol = "user";
 
 // v.1.0.0 -> change link from native app to pwa
 // v.1.1.0 -> add role access rights
+// v.1.2.0 -> change link from pwa to native app
 
 exports.setup =
 {
 	on: true,
 	title: 'qrcode',
 	description: "A plugin to manage the QRCODEs",
-	version: '1.1.0',
+	version: '1.2.0',
 }
 
 async function getAdminById(_id) {
@@ -41,57 +43,57 @@ async function getAdminById(_id) {
 }
 
 async function createImage(_target) {
-	return new Promise(resolve => {
+	return new Promise((resolve, reject) => {
 		QRCode.toDataURL(_target, function (err, url) {
-			resolve(url);
+			if (err) {
+				reject(err);
+			} else {
+				resolve(url);
+			}
 		});
 	});
 }
 
 async function delQRCODE(_id) {
-	if (!_id) {
-		resolve();
-		return;
-	}
-	var _search =
-	{
-		id: _id,
-	};
-
-	return new Promise(resolve => {
-		STORE.db.linkdb.Delete(_qrCol, _search, function (_err, _data) {
-			resolve(_data);
+	return new Promise((resolve, reject) => {
+		if (!_id) {
+			reject(new Error(`delQRCODE invalid param '_id' provided [${_id}]`));
+		}
+		const _search = { id: _id };
+		STORE.db.linkdb.Delete(_qrCol, _search, function (err, _data) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(_data);
+			}
 		});
 	});
 }
 
-
 async function getQRCODE(_id) {
-	var _search = {};
-	if (_id) {
-		_search.id = _id;
-	}
-	return new Promise(resolve => {
+	return new Promise((resolve, reject) => {
+		const _search = {};
+		if (_id) {
+			_search.id = _id;
+		}
 		STORE.db.linkdb.Find(_qrCol, _search, null, function (_err, _data) {
 			if (_data) {
 				resolve(_data);
-			}
-			else {
-				resolve(_err);
+			} else {
+				reject(_err);
 			}
 		});
 	});
 }
 
 async function saveQRCODE(_obj) {
-	return new Promise(resolve => {
+	return new Promise((resolve, reject) => {
 		_obj.updatedAt = Date.now();
 		STORE.db.linkdb.Save(_qrCol, _obj, function (_err, _data) {
 			if (_data) {
 				resolve(_data);
-			}
-			else {
-				resolve(_err);
+			} else {
+				reject(_err);
 			}
 		});
 	});
@@ -103,6 +105,7 @@ exports.plugin =
 	desc: "Manage QRCODES",
 	role: "admin",
 	handler: async (req, res) => {
+		// Check access rights
 		const admin = await getAdminById(req.userCookie.data.id);
 		if (!verifyRoleAccess(admin?.data?.roleBackOffice, AUTHORIZED_ROLES)) {
 			res.writeHead(401);
@@ -120,6 +123,8 @@ exports.plugin =
 			const qrCodeObj = {
 				title: req.post.title,
 				appLink: req.post.appLink,
+				apple: req.post.apple,
+				android: req.post.android,
 				createdAt: Date.now(),
 				updatedAt: Date.now(),
 			}
@@ -140,7 +145,9 @@ exports.plugin =
 			var _tmp = _qr;
 			_tmp = _tmp.replace(/__ID__/g, _data[i].id);
 			_tmp = _tmp.replace(/__TITLE__/g, _data[i].title);
-			_tmp = _tmp.replace(/__APP_LINK__/g, _data[i].appLink || '-');
+			_tmp = _tmp.replace(/__APP_LINK__/g, _data[i]?.appLink || '-');
+			_tmp = _tmp.replace(/__APPLE__/g, _data[i]?.apple || '-');
+			_tmp = _tmp.replace(/__ANDROID__/g, _data[i]?.android || '-');
 			_tmp = _tmp.replace(/__IMG__/g, await createImage(`${OPTION.HOST_BASE_URL}/${_qrCol}/${_data[i].id}`));
 			_tmp = _tmp.replace(/__URL__/g, `${OPTION.HOST_BASE_URL}/${_qrCol}/${_data[i].id}`);
 			_list += _tmp;
@@ -156,11 +163,16 @@ exports.plugin =
  */
 async function qrHandler(req, res) {
 	/**@type {qrcode} */
-	var [_result] = await getQRCODE(req.param.id);
+	const _result = await getQRCODE(req.param.id);
 
-	console.log('QRCODE READIRECT TO', `${OPTION.HOST_BASE_URL}${_result.appLink}`)
 	if (_result) {
-		UTILS.Redirect(res, `${OPTION.HOST_BASE_URL}${_result.appLink}`);
+		res.setHeader("Content-Type", "text/html; charset=utf-8");
+		res.end(
+			_landing
+				.replace("__TITLE__", _result[0].title)
+				.replaceAll("__APPLE__", _result[0]?.apple || '/')
+				.replaceAll("__ANDROID__", _result[0]?.android || '/')
+		);
 	}
 	else {
 		UTILS.Redirect(res, "/");
