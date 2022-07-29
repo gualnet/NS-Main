ab01f2e77f35e963741dd19fc654bc923a69c0d0
Gualnet <galy@student.42.fr>
2022-07-29 16:15:19 +0200

    add eResa login endpoint

---
+++ snap/magelan_api/index.js
@@ -0,0 +1,53 @@
+const axios = require('axios').default;
+
+exports.setup = {
+	on: true,
+  title: 'Magelan Api External',
+  description: 'proxy to interface magelan eresa api',
+  version: '1.0.0',
+  api: true,
+}
+
+const loginHandler = async (req, res) => {
+	try {
+		const login = req.get.login;
+		const pass = req.get.password;
+		const keyUser = OPTION.MAGELAN_USER_KEY;
+
+		const url = `https://appli.magelan-eresa.com/appliLogin/${login}/${pass}/${keyUser}`;
+
+		const response = await axios.get(url);
+		if (response.data.CodeErr === '2') {
+			throw new Error(response.data?.MessageErr || 'Unknown Internal Error');
+		}
+		const resData = {
+			login: response.data.res_login,
+			token: response.data.res_token,
+		}
+		res.writeHead(response.status, response.statusText, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({
+			success: true,
+			data: resData,
+		}));
+	} catch (error) {
+		console.error(error)
+		res.writeHead(500, 'Error', { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({
+			success: false,
+			error: error.toString(),
+		}));
+	}
+};
+
+exports.router = [
+	{
+    on: true,
+    route: "/api/eresa/login",
+    method: "GET",
+    handler: loginHandler
+  },
+];
+
+exports.handler = async(req, res) => { 
+ res.end('Hello Snap!'); 
+}
