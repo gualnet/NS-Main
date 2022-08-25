ada39a605c7312cb2da687cfe1d5a1670f92bcca
Gualnet <galy@student.42.fr>
2022-08-25 18:44:31 +0200

    add myLogger in libs

---
+++ snap/lib-js/myLogger.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+/**
+ * 
+ * @param {*} error 
+ * @param {logErrorOptions} options 
+ */
+function logError(error, options) {
+
+	const FILE_NAME = `log_error_${new Date().toLocaleDateString()}.txt`;
+	// console.log('FILE_NAME', FILE_NAME);
+	const FILE_PATH = `${__dirname}/../${FILE_NAME}`;
+	// console.log('FILE_PATH', FILE_PATH);
+
+
+	const msg = `- ERROR [${new Date().toLocaleString('fr-FR')}] [${options.module}]\n => [${error.stack || error}]\n\n`;
+	fs.appendFileSync(FILE_PATH, msg);
+};
+
+module.exports = {
+	logError,
+}
+
+/**
+ * @typedef logErrorOptions
+ * @property {string} module - origin snap name
+ * 
+ */
